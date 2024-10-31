734bd8283455e377e540c80e3cb83990e6e1391b
Yasinalmaz <yasinalmaz1334@gmail.com>
2024-11-01 02:20:25 +0300

    Add files via upload

---
+++ odev1,0/Program.cs
@@ -0,0 +1,202 @@
+﻿//DEgisken kullanimi ve deger okuma
+
+/*
+//Ornek1
+// Hosgeldiniz Nesneye yonelik programlama dersine yazalım.
+string a = "Hosgeldizin ";
+string b = "Nesneye ";
+string c = "Yonelik ";
+string d = "Programlama ";
+string e = "Dersine.";
+Console.WriteLine(a + b + c + d + e); 
+*/
+
+
+/*
+//Ornek2
+//Ders ogretmenimiz Doktor Arzu Kilitci Calayir yazalım.
+string a = "Ders ";
+string b = "Ogretmenimiz ";
+string c = "Doktor ";
+string d = "Arzu  ";
+string e = "Kilitci.";
+string f = "Calayir.";
+
+Console.WriteLine(a + b + c + d + e + f);
+*/
+
+
+/*
+//Ornek3
+// Yasin Almaz Bilgisayar Muhendisligi Ogrencisi 23 yasında 1.80 boyunda yazalım.
+string ad = "Yasin";
+string soyad = "Almaz";
+string ders_ogrencisi = "Bilgisayar Muhendisligi ogrencisi";
+int yas = 23;
+double boy = 1.80 ;
+Console.WriteLine(ad + soyad + ders_ogrencisi + yas + "yasinda " + boy + "boyunda");
+*/
+
+/*
+//Ornek4
+//29 Ekim 1923 Cumhuriyetin İlanı
+int a = 29;
+string b = "Ekim";
+int c = 1923;
+string d = "Cumhuriyetin Ilanı.";
+Console.WriteLine(a + b + c + d);   
+*/
+
+
+/*
+//Ornek5
+//Klavyeden kişiye ait bilgiler alalım.
+Console.WriteLine("ad: ");
+string ad = Console.ReadLine();
+Console.WriteLine("soyad: ");
+string soyad = Console.ReadLine();
+Console.WriteLine("universite: ");
+string universite = Console.ReadLine();
+Console.WriteLine("bolum: ");
+string bolum = Console.ReadLine();
+Console.WriteLine("sinifi: ");
+int sinif = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(ad + soyad + " Isimli " + universite + " Universitesi " + bolum + " Bolumu " + sinif + " Sinifli ogrencisiyim.");
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+//Iki sayinin farkini bulma
+// 85 - 47 yapalim
+int a = 85;
+int b = 47;
+int c = a - b;
+Console.WriteLine("Iki sayinin farki: " + c );
+*/
+
+/*
+//Sayinin karesini bulalim
+//Ornek1
+//4 sayisinin karesi?
+int a = 4;
+int b = a * a;
+Console.WriteLine("4 un karesi: " + b);
+*/
+
+/*
+//Ornek2
+//16 sayisinin karesi?
+int a = 16;
+int b = a * a;
+Console.WriteLine("16 nın karesi: " + b);
+*/
+
+/*
+//Ornek3
+//33 sayisinin karesi?
+int a = 33;
+int b = a * a;
+Console.WriteLine("33 un karesi: " + b);
+*/
+
+/*
+//Ornek4
+//149 sayisinin karesi?
+int a = 149;
+int b = a * a;
+Console.WriteLine("149 un karesi: " + b);
+*/
+
+/*
+//Klavyeden girilen sayinin karesi
+Console.WriteLine("Sayiyi giriniz: ");
+int sayi = (int)Convert.ToUInt32(Console.ReadLine());
+int karesi = sayi * sayi;                                                ????????????????????????????????????
+Console.WriteLine("Girilen sayinin karesi: " + karesi);
+*/
+
+
+/*
+//10 sayisinin 3 bolumunden kalanını bulma.
+int a = 10;
+int b = 3;
+int c = a % b;
+Console.WriteLine("10 sayisinin 3 e bolumunden kalan: " + c);
+*/
+
+/*
+//Klavyeden girilen 4 sayinin toplami ve carpimini bulalim
+
+Console.WriteLine("1. sayiyi giriniz: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("2. sayiyi giriniz: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("3. sayiyi giriniz: ");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("4. sayiyi giriniz: ");
+int d = Convert.ToInt32(Console.ReadLine());
+
+int toplam = a + b + c + d;
+int carpim = a * b * c * d;
+
+Console.WriteLine("Girilen 4 sayinin toplami: " + toplam );
+Console.WriteLine("Girilen 4 sayinin carpimi: " + carpim );
+*/
+
+/*
+//Klavyeden girilen iki sayinin bolumunu bulalim
+Console.WriteLine("Ilk sayiyi giriniz: ");
+int sayi_1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ikinci sayiyi giriniz: ");
+int sayi_2 = Convert.ToInt32(Console.ReadLine());
+int bolum = sayi_1 / sayi_2;
+Console.WriteLine("Klavyeden girilen iki sayinin bolumu: " + bolum);
+*/
+
+/*
+//Dikdortgenler Prizmasinin hacmini bulalim.
+//Dikdortgenler prizmasi hacmi en * boy * yukseklikten bulunur
+
+Console.WriteLine("Dikdortgenin enini giriniz(cm cinsinden): ");
+int en = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Dikdortgenin boyunu giriniz(cm cinsinden): ");
+int boy = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Dikdortgenin yuksekligini giriniz(cm cinsinden): ");
+int yukseklik = Convert.ToInt32(Console.ReadLine());
+int hacim = en * boy * yukseklik;
+Console.WriteLine("Dıkdortgenler prizmasinin hacmi: " + hacim + "cm'kuptur");
+
+*/
+
+
+
+
+
+
+
+
+
+
+
